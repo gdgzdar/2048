bffd1e0d50a17bee7583de5740dd24f7d7ec52da
Marek Kadlčík <makadlcik@gmail.com>
2015-06-22 22:01:27 +0200

    text version - initial

--- text-edition-game.js
+++ text-edition-game.js
@@ -1,4 +1,11 @@
-//document.write("why?");
+// document.write("why?"); // for testing only
+
+var gameField = [	[	[0, true], [0, true], [0, true], [0, true]	],
+					[	[0, true], [0, true], [0, true], [0, true]	],
+					[	[0, true], [0, true], [0, true], [0, true]	],
+					[	[0, true], [0, true], [0, true], [0, true]	]	];
+var score = 0;
+writeToDocument();
 
 function moveUp() {
 
@@ -30,8 +37,33 @@ function isOver() {
 
 function addCell() {
 
+	var emptyPositions = [[]];
+
+	for (index = 0; index < gameField.length; index++) {
+		for (insiderIndex = 0; insiderIndex < gameField[index].length - 1; insiderIndex++) {
+			if (gameField[index][insiderIndex][0] == 0) {
+				emptyPositions.add([index, insiderIndex]);
+			}
+		}
+	}
+
+	var randomPositionIndex = Math.floor(Math.random() * (emptyPositions.length));
+
+	// we ended here
 }
 
-function addScore() {
+
+// write output
+function writeToDocument() {
+
+	var node = document.getElementById('gameBody');
+
+	node.innerHTML = "score: " + score + "<br>";
+	for (index = 0; index < gameField.length; index++) {
+		for (insiderIndex = 0; insiderIndex < gameField[index].length - 1; insiderIndex++) {
+			node.innerHTML += gameField[index][insiderIndex][0] + " , ";
+		}
+		node.innerHTML += gameField[index][gameField[index].length-1][0] + "</br>";
+	}
 
 }
