70be58e4aed8051589171194a2d700b625f790bc
Failos <horak.matej.zdar@gmail.com>
2015-06-25 14:15:11 +0200

    text version 0.1

--- text-edition-game.js
+++ text-edition-game.js
@@ -5,7 +5,7 @@ var gameField = [	[	[0, true], [0, true], [0, true], [0, true]	],
 					[	[0, true], [0, true], [0, true], [0, true]	],
 					[	[0, true], [0, true], [0, true], [0, true]	]	];
 var score = 0;
-writeToDocument();
+addCell();
 
 function moveUp() {
 
@@ -35,30 +35,43 @@ function isOver() {
 
 }
 
+// Add new number into empty cell. 2 or 4...
 function addCell() {
 
-	var emptyPositions = [[]];
+	// Array of empty cells in gamefield
+	var emptyPositions = [];
 
+	// Fill array with empty cells positions
 	for (index = 0; index < gameField.length; index++) {
-		for (insiderIndex = 0; insiderIndex < gameField[index].length - 1; insiderIndex++) {
+		for (insiderIndex = 0; insiderIndex < gameField[index].length; insiderIndex++) {
 			if (gameField[index][insiderIndex][0] == 0) {
-				emptyPositions.add([index, insiderIndex]);
+				emptyPositions.push([index, insiderIndex]);
 			}
 		}
 	}
 
-	var randomPositionIndex = Math.floor(Math.random() * (emptyPositions.length));
+	var randomPositionIndex = Math.floor(Math.random() * (emptyPositions.length)); // Get random empty cell
+	var cellToAdd = (Math.random() <= 0.1) ? 4 : 2; // 2 or 4 in correct ratio
 
-	// we ended here
+
+	var positionY = emptyPositions[randomPositionIndex][0]; // Row of changed cell
+	var positionX = emptyPositions[randomPositionIndex][1]; // Column of changed cell
+
+	gameField[positionY][positionX][0] = cellToAdd; // Add
+
+	writeToDocument(); // Refresh displayed gamefield
 }
 
 
-// write output
+
+// Write output
 function writeToDocument() {
 
-	var node = document.getElementById('gameBody');
+	var node = document.getElementById('gameBody'); // Get HTML element
 
-	node.innerHTML = "score: " + score + "<br>";
+	node.innerHTML = "score: " + score + "<br>"; // Write score
+
+	// Write game field in correct format
 	for (index = 0; index < gameField.length; index++) {
 		for (insiderIndex = 0; insiderIndex < gameField[index].length - 1; insiderIndex++) {
 			node.innerHTML += gameField[index][insiderIndex][0] + " , ";
