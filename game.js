57d921be7886813ffa6996e5e07f6c60aa0f8986
HormCZ <horak.matej.zdar@gmail.com>
2016-02-18 17:02:05 +0100

    Add karma JS tests, refactor

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
-var gameField = [   [   [0, true], [0, true], [0, true], [0, true]  ],
-					[   [0, true], [0, true], [0, true], [0, true]  ],
-					[   [0, true], [0, true], [0, true], [0, true]  ],
-					[   [0, true], [0, true], [0, true], [0, true]  ]   ];
+var gameField = [
+  [[0, true], [0, true], [0, true], [0, true]],
+  [[0, true], [0, true], [0, true], [0, true]],
+  [[0, true], [0, true], [0, true], [0, true]],
+  [[0, true], [0, true], [0, true], [0, true]]];
 var score = 0;
 var targetNumber = 2048;
 var scoreLabel = document.getElementById('score-inner-value'); // Get HTML element
@@ -9,235 +10,226 @@ const number = 0; // Not a good name for variable, is it?
 const state = 1; // If a tile can be modified...
 
 function cleanStates() {
-	for (var i = 0; i < gameField.length; i++) {
-		for (var j = 0; j < (gameField[i]).length; j++) {
-			gameField[i][j][state] = true;
-		}
-	}
+  for (var i = 0; i < gameField.length; i++) {
+    for (var j = 0; j < (gameField[i]).length; j++) {
+      gameField[i][j][state] = true;
+    }
+  }
 }
 
 function moveDone() {
-	cleanStates();
-	refresh();
-	if (isWon()) {
-		alert("You win this game! :)");
-	} else if (isOver()) {
-		alert("You lose this game! :(");
-	} else {
-		addCell();
-	}
+  cleanStates();
+  refresh();
+  if (isWon()) {
+    alert("You win this game! :)");
+  } else if (isOver()) {
+    alert("You lose this game! :(");
+  } else {
+    addCell();
+  }
+}
+
+function canBeMerged(alongsideTile, movedTile) {
+  return (alongsideTile[state] && movedTile[state]) &&
+      (alongsideTile[number] === 0 || alongsideTile[number] === movedTile[number]);
 }
 
 // Move tile, merge and return boolean if the merge is possible
 function moveStep(alongsideTile, movedTile) {
 
-	if (alongsideTile[state] && movedTile[state]) { // If can be merged...
-		if (alongsideTile[number] === 0) { // if the alongside tile is empty
-			alongsideTile[number] = movedTile[number];
-			movedTile[number] = 0;
-			return true; // can continue trying to move
+  if (canBeMerged(alongsideTile, movedTile)) {
+    alongsideTile[state] = false;
+    alongsideTile[number] += movedTile[number];
+    movedTile[number] = 0;
+    score += alongsideTile[number];
+    return true; // can continue trying to move
+  }
 
-		} else if (alongsideTile[number] === movedTile[number]) { // if the alongside tile and moved tile are same
-			alongsideTile[state] = false;
-			alongsideTile[number] *= 2;
-			movedTile[number] = 0;
-			score += alongsideTile[number];
-			return true; // can continue trying to move
-		}
-
-		 
-	}
-	return false; // cannot continue trying to move
+  return false; // cannot continue trying to move
 }
 
 function moveUp() {
 
-	for (var i = 1; i < gameField.length; i++) { // This loop starts on the second row and goes down...
+  for (var i = 1; i < gameField.length; i++) { // This loop starts on the second row and goes down...
 
-		for (var j = 0; j < (gameField[i]).length; j++) { // This loop starts on the first column and goes right...
-			
-			// For HTML redraw
-			var originalPosition = i;
-			var steps = 0;
-			var originalValue;	// save value for merge HTML divs
+    for (var j = 0; j < (gameField[i]).length; j++) { // This loop starts on the first column and goes right...
 
-			// This loop moving a tile as long as it's possible
-			for (var k = i; k > 0; k--) {
-				originalValue = gameField[i - 1] [j][number];
-				if (!moveStep(gameField[k - 1] [j], gameField[k] [j])) {
-					break;
-				}
+      // For HTML redraw
+      var originalPosition = i;
+      var steps = 0;
+      var originalValue;	// save value for merge HTML divs
 
-				steps++;
-			}
+      // This loop moving a tile as long as it's possible
+      for (var k = i; k > 0; k--) {
+        originalValue = gameField[i - 1] [j][number];
+        if (!moveStep(gameField[k - 1] [j], gameField[k] [j])) {
+          break;
+        }
 
-			// HTML redrawing
-			var newPosition = originalPosition - steps;
-			$(".cell.row-" + originalPosition + ".column-" + j).each(function() { // Find all elements at this position
-				// Change position of tile with animation and rewrite value in span
-				if (originalValue === gameField[newPosition][j][number]) { // If
-					$(".cell.row-" + newPosition + ".column-" + j).remove();
-				}
+        steps++;
+      }
 
-				$(this).removeClass("row-" + originalPosition);
-				$(this).addClass("row-" + newPosition);
-				this.firstChild.innerHTML = gameField[newPosition][j][number]; // Rewrite value in span
-			});
+      // HTML redrawing
+      var newPosition = originalPosition - steps;
+      $(".cell.row-" + originalPosition + ".column-" + j).each(function() { // Find all elements at this position
+        // Change position of tile with animation and rewrite value in span
+        if (originalValue === gameField[newPosition][j][number]) { // If
+          $(".cell.row-" + newPosition + ".column-" + j).remove();
+        }
 
-		}
-	}
-	moveDone();
-	node = document.getElementById("textField");
-	node.innerHTML = "";
-	for (index = 0; index < gameField.length; index++) {
-		for (insiderIndex = 0; insiderIndex < gameField[index].length - 1; insiderIndex++) {
-			node.innerHTML += gameField[index][insiderIndex][number] + " ,"  //+ "(" + gameField[index][insiderIndex][state] + ") , ";
-		}
-		node.innerHTML += gameField[index][gameField[index].length-1][number]+ "</br>";
-	}
+        $(this).removeClass("row-" + originalPosition);
+        $(this).addClass("row-" + newPosition);
+        this.firstChild.innerHTML = gameField[newPosition][j][number]; // Rewrite value in span
+      });
+
+    }
+  }
+  moveDone();
+  node = document.getElementById("textField");
+  node.innerHTML = "";
+  for (index = 0; index < gameField.length; index++) {
+    for (insiderIndex = 0; insiderIndex < gameField[index].length - 1; insiderIndex++) {
+      node.innerHTML += gameField[index][insiderIndex][number] + " ,"  //+ "(" + gameField[index][insiderIndex][state] + ") , ";
+    }
+    node.innerHTML += gameField[index][gameField[index].length - 1][number] + "</br>";
+  }
 }
-
 
 function moveDown() {
-	for (var i = 2; i >= 0; i--) { // This loop starts on the third row and goes up...
-		for (var j = 0; j < (gameField[i]).length; j++) { // This loop starts on the first column and goes right...
-			for (var k = i; k < gameField.length - 1; k++) {
+  for (var i = 2; i >= 0; i--) { // This loop starts on the third row and goes up...
+    for (var j = 0; j < (gameField[i]).length; j++) { // This loop starts on the first column and goes right...
+      for (var k = i; k < gameField.length - 1; k++) {
 
-				if (!moveStep(gameField[k + 1][j], gameField[k][j])) {
-					break;
-				}
+        if (!moveStep(gameField[k + 1][j], gameField[k][j])) {
+          break;
+        }
 
-			}
-		}
-	}
-	moveDone();
+      }
+    }
+  }
+  moveDone();
 }
-
 
 function moveLeft() {
-	for (var i = 1; i < gameField.length; i++) { // This loop starts on the second row and goes down...
-		for (var j = 0; j < (gameField[i]).length; j++) { // This loop starts on the first column and goes right...
-			for (var k = i; k > 0; k--) {
-				refresh();
-				if (!moveStep(gameField[j][k - 1], gameField[j][k])) {
-					break;
-				}
+  for (var i = 1; i < gameField.length; i++) { // This loop starts on the second row and goes down...
+    for (var j = 0; j < (gameField[i]).length; j++) { // This loop starts on the first column and goes right...
+      for (var k = i; k > 0; k--) {
+        refresh();
+        if (!moveStep(gameField[j][k - 1], gameField[j][k])) {
+          break;
+        }
 
-			}
-		}
-	}
-	moveDone();
+      }
+    }
+  }
+  moveDone();
 }
-
 
 function moveRight() {
-	for (var i = 2; i >= 0; i--) { // This loop starts on the third row and goes up...
-		for (var j = 0; j < (gameField[i]).length; j++) { // This loop starts on the first column and goes right...
-			for (var k = i; k < gameField.length - 1; k++) {
+  for (var i = 2; i >= 0; i--) { // This loop starts on the third row and goes up...
+    for (var j = 0; j < (gameField[i]).length; j++) { // This loop starts on the first column and goes right...
+      for (var k = i; k < gameField.length - 1; k++) {
 
-				if (!moveStep(gameField[j][k + 1], gameField[j][k])) {
-					break;
-				}
+        if (!moveStep(gameField[j][k + 1], gameField[j][k])) {
+          break;
+        }
 
-			}
-		}
-	}
-	moveDone();
+      }
+    }
+  }
+  moveDone();
 }
-
 
 // restart the game
 function newGame() {
-	for (index = 0; index < gameField.length; index++) {
-		for (insiderIndex = 0; insiderIndex < gameField[index].length; insiderIndex++) {
-			gameField[index][insiderIndex][0] = 0;
-		}
-	}
-	var gameFieldElement = document.getElementById("game-field");
-	while (gameFieldElement.firstChild) {
-	    gameFieldElement.removeChild(gameFieldElement.firstChild);
-	}
-	score = 0;
-	refresh();
+  for (index = 0; index < gameField.length; index++) {
+    for (insiderIndex = 0; insiderIndex < gameField[index].length; insiderIndex++) {
+      gameField[index][insiderIndex][0] = 0;
+    }
+  }
+  var gameFieldElement = document.getElementById("game-field");
+  while (gameFieldElement.firstChild) {
+    gameFieldElement.removeChild(gameFieldElement.firstChild);
+  }
+  score = 0;
+  refresh();
 }
 
 // returns if given number is in gamefield
 function isInGameField(number) {
-	for (index = 0; index < gameField.length; index++) {
-		for (insiderIndex = 0; insiderIndex < gameField[index].length; insiderIndex++) {
-			if (gameField[index][insiderIndex][0] == number) {
-				return true;
-			}
-		}
-	}
-	return false;
+  for (index = 0; index < gameField.length; index++) {
+    for (insiderIndex = 0; insiderIndex < gameField[index].length; insiderIndex++) {
+      if (gameField[index][insiderIndex][0] == number) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
-
 
 // returns if game is won
 function isWon() {
-	return isInGameField(targetNumber);
+  return isInGameField(targetNumber);
 }
 
-
-// returns if game is over 
+// returns if game is over
 function isOver() {
-	if (isInGameField(0)) {
-		return false;
-	}
-	
-	else {
-		for (index = 0; index < gameField.length - 1; index++) {
-			for (insiderIndex = 0; insiderIndex < gameField[index].length - 1; insiderIndex++) {
-				if (gameField[index][insiderIndex][0] == gameField[index][insiderIndex + 1][0] || gameField[index][insiderIndex][0] == gameField[index + 1][insiderIndex][0]) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-		
-}
+  if (isInGameField(0)) {
+    return false;
+  }
 
+  else {
+    for (index = 0; index < gameField.length - 1; index++) {
+      for (insiderIndex = 0; insiderIndex < gameField[index].length - 1; insiderIndex++) {
+        if (gameField[index][insiderIndex][0] == gameField[index][insiderIndex + 1][0] || gameField[index][insiderIndex][0] == gameField[index + 1][insiderIndex][0]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+}
 
 // Add new number into an empty cell. 2 or 4...
 function addCell() {
 
-	// Array of empty cells in the gamefield
-	var emptyPositions = [];
+  // Array of empty cells in the gamefield
+  var emptyPositions = [];
 
-	// Fill an array with empty cells positions
-	for (index = 0; index < gameField.length; index++) {
-		for (insiderIndex = 0; insiderIndex < gameField[index].length; insiderIndex++) {
-			if (gameField[index][insiderIndex][0] == 0) {
-				emptyPositions.push([index, insiderIndex]);
-			}
-		}
-	}
+  // Fill an array with empty cells positions
+  for (index = 0; index < gameField.length; index++) {
+    for (insiderIndex = 0; insiderIndex < gameField[index].length; insiderIndex++) {
+      if (gameField[index][insiderIndex][0] == 0) {
+        emptyPositions.push([index, insiderIndex]);
+      }
+    }
+  }
 
-	var randomPositionIndex = Math.floor(Math.random() * (emptyPositions.length)); // Get random empty cell
-	var cellToAdd = (Math.random() <= 0.1) ? 4 : 2; // 2 or 4 in correct ratio
+  var randomPositionIndex = Math.floor(Math.random() * (emptyPositions.length)); // Get random empty cell
+  var cellToAdd = (Math.random() <= 0.1) ? 4 : 2; // 2 or 4 in correct ratio
 
-	var positionY = emptyPositions[randomPositionIndex][0]; // Row of changed cell
-	var positionX = emptyPositions[randomPositionIndex][1]; // Column of changed cell
+  var positionY = emptyPositions[randomPositionIndex][0]; // Row of changed cell
+  var positionX = emptyPositions[randomPositionIndex][1]; // Column of changed cell
 
+  gameField[positionY][positionX][0] = cellToAdd; // Add
 
-	gameField[positionY][positionX][0] = cellToAdd; // Add
+  var cellWrapper = document.createElement("div");
+  var cellText = document.createElement("span");
+  cellText.innerHTML = cellToAdd;
+  cellWrapper.className = "cell box column-" + positionX + " row-" + positionY;
+  cellWrapper.appendChild(cellText);
+  document.getElementById("game-field").appendChild(cellWrapper);
 
-	var cellWrapper = document.createElement("div");
-	var cellText = document.createElement("span");
-	cellText.innerHTML = cellToAdd;
-	cellWrapper.className = "cell box column-" + positionX + " row-" + positionY;
-	cellWrapper.appendChild(cellText);
-	document.getElementById("game-field").appendChild(cellWrapper);
-
-	refresh(); // Refresh displayed gamefield
+  refresh(); // Refresh displayed gamefield
 
 }
 
 function refresh() {
-    scoreLabel.innerHTML = score; // Write score
+  scoreLabel.innerHTML = score; // Write score
 }
 
-addCell();
-refresh();
+function start() {
+  addCell();
+  refresh();
+}
---
+++ game.spec.js
@@ -0,0 +1,95 @@
+describe("game", function() {
+
+  describe("canBeMerged", function() {
+
+
+    it("should not merge different values", function() {
+
+      var alongsideTile = [2, true];
+      var movedTile = [4, true];
+
+      expect(canBeMerged(alongsideTile, movedTile)).toBe(false);
+    });
+
+    it("should not merge different states", function() {
+
+      var alongsideTile = [2, false];
+      var movedTile = [2, true];
+
+      expect(canBeMerged(alongsideTile, movedTile)).toBe(false);
+    });
+
+    it("should not merge different states and values", function() {
+
+      var alongsideTile = [2, false];
+      var movedTile = [4, true];
+
+      expect(canBeMerged(alongsideTile, movedTile)).toBe(false);
+    });
+
+    it("should merge same states and values", function() {
+
+      var alongsideTile = [2, true];
+      var movedTile = [2, true];
+
+      expect(canBeMerged(alongsideTile, movedTile)).toBe(true);
+
+    });
+
+    it("should merge with 0", function() {
+
+      var alongsideTile = [0, true];
+      var movedTile = [2, true];
+
+      expect(canBeMerged(alongsideTile, movedTile)).toBe(true);
+
+    });
+  });
+
+  describe("moveStep", function() {
+    var origCanBeMerged;
+
+    beforeEach(function() {
+      origCanBeMerged = canBeMerged;
+    });
+    
+    afterEach(function() {
+      canBeMerged = origCanBeMerged;
+    });
+
+    function checkValuesIsZero(movedTile) {
+      expect(movedTile[number]).toBe(0);
+    }
+
+    it("should not merge non-mergeble tiles", function() {
+
+      canBeMerged = function () {
+        return false;
+      };
+
+      var alongsideTile = [2, true];
+      var movedTile = [4, true];
+
+      expect(moveStep(alongsideTile, movedTile)).toBe(false);
+      expect(alongsideTile).toEqual([2, true]);
+      expect(movedTile).toEqual([4, true]);
+    });
+
+    it("should merge if are mergeble", function() {
+
+      canBeMerged = function () {
+        return true;
+      };
+
+      var alongsideTile = [2, true];
+      var movedTile = [2, true];
+
+      expect(moveStep(alongsideTile, movedTile)).toBe(true);
+      expect(alongsideTile).toEqual([4, false]);
+      expect(movedTile).toEqual([0, true]);
+    });
+
+
+  });
+
+});
